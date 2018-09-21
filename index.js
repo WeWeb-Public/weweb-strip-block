12fab2ebcf8c172af1173bafa2007a596b7f33ec
Kramerbaf <nds.kram@gmail.com>
2018-09-21 12:02:56 +0200

    Changed regex

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var loaderUtils = require("loader-utils");
 
 function StripBlockLoader(content) {
     var options = loaderUtils.getOptions(this) || {};
-    var startComment = options.start || 'develblock:start';
-    var endComment = options.end || 'develblock:end';
+    var startComment = options.start || 'wwManager:start';
+    var endComment = options.end || 'wwManager:end';
 
+    /* OLD REGEX 
     var regexPattern = new RegExp("[\\t ]*\\/\\* ?" + startComment + " ?\\*\\/[\\s\\S]*?\\/\\* ?" + endComment + " ?\\*\\/[\\t ]*\\n?", "g");
+    */
+    var regexPattern = new RegExp("[\\t ]*(?:\\/\\*|<!--) ?" + startComment + " ?(?:\\*\\/|-->)[\\s\\S]*?(?:\\/\\*|<!--) ?" + endComment + " ?(?:\\*\\/|-->)[\\t ]*\\n?", "g");
 
     content = content.replace(regexPattern, '');
 
